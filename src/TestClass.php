ab2fcfe0650cba6576ae8d3fc95416ab29f98504
sam <samdrum12@gmail.com>
2024-01-10 16:00:28 +0100

    testing rules

--- src/TestClass.php
+++ src/TestClass.php
@@ -2,7 +2,7 @@
 
 class TestClass
 {
-    public function printHello()
+    public function printHello($myHelperArray)
     {
         #TODO: this is a line for testing the linter
         print "hello world";
